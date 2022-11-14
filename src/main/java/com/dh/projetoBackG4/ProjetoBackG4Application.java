d274a7bb8dc16ca36eed8443d34ab7071464d1c5
Sabrina <freibergerssb@gmail.com>
2022-11-14 20:05:46 -0300

    Criando projeto com SpringBoot

---
+++ src/main/java/com/dh/projetoBackG4/ProjetoBackG4Application.java
@@ -0,0 +1,13 @@
+package com.dh.projetoBackG4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBackG4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBackG4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dh/projetoBackG4/ProjetoBackG4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.projetoBackG4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBackG4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
