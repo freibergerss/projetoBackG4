21eb78b01b068c5e6f5df89d9ee5e7c127f4bcfb
roger <dh.rogeriosilva@gmail.com>
2022-11-14 20:51:16 -0300

    Criação da Classe Dentista.

---
+++ src/main/java/model/Dentista.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Dentista {
+    private String nome;
+    private String sobrenome;
+    private int idDentista;
+
+    public Dentista(String nome, String sobrenome, int idDentista) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.idDentista = idDentista;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getIdDentista() {
+        return idDentista;
+    }
+
+    public void setIdDentista(int idDentista) {
+        this.idDentista = idDentista;
+    }
+}
+
