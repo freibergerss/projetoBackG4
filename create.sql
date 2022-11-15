4170c2f5763908ebcc016295ae28ee0944ab3524
Everton Silva <everton.p.silva@outlook.com>
2022-11-14 23:59:31 -0300

    Implementando coisinhas

--- create.sql
+++ create.sql
@@ -1,5 +1,15 @@
-CREATE TABLE IF NOT EXISTS dentista(
-id INT auto_increment PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS dentistas(
+idDentista INT auto_increment PRIMARY KEY,
 nome VARCHAR(255) NOT NULL,
-sobrenome VARCHAR(255) NOT NULL,
+sobrenome VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS pacientes(
+
+idPaciente INT auto_increment PRIMARY KEY,
+nome VARCHAR(255) NOT NULL,
+sobrenome VARCHAR(255),
+rg VARCHAR(15) NOT NULL,
+endereco VARCHAR(350),
+dataCadastro DATE NOT NULL
 );
