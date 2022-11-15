6924c51e68eaeed6cad85f1dc86c94ad07e00bde
Everton Silva <everton.p.silva@outlook.com>
2022-11-15 12:53:05 -0300

    Tentando implementar o método Update e o método de buscar o paciente por Id.

--- src/main/java/com/dh/projetoBackG4/dao/IDao.java
+++ src/main/java/com/dh/projetoBackG4/dao/IDao.java
@@ -1,9 +1,19 @@
 package com.dh.projetoBackG4.dao;
 
+import model.Paciente;
+
 import java.sql.SQLException;
+import java.util.Optional;
 
 public interface IDao <T>{
-    public T create (T t) throws SQLException;
-    public T update (T t) throws SQLException;
+    T create(T t) throws SQLException;
+    T update(T t) throws SQLException;
+    void deletePaciente(int idPaciente);
+
+    Optional<Paciente> buscarPorId(int idPaciente) throws SQLException;
+
+
+
+
 
 }
--- src/main/java/com/dh/projetoBackG4/dao/impl/ImplPacienteDaoH2.java
+++ src/main/java/com/dh/projetoBackG4/dao/impl/ImplPacienteDaoH2.java
@@ -5,6 +5,8 @@ import com.dh.projetoBackG4.dao.IDao;
 import model.Paciente;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.util.Optional;
 
 public class ImplPacienteDaoH2 implements IDao<Paciente> {
 
@@ -54,6 +56,70 @@ public class ImplPacienteDaoH2 implements IDao<Paciente> {
 
     @Override
     public Paciente update(Paciente paciente) throws SQLException {
-        return null;
+        Connection connection = null;
+//        String SQLUpdate = String.format("UPDATE pacientes SET nome = ?, sobrenome = ?, rg = ?, endereco = ?, dataCadastro = ? WHERE idPaciente = ?;");
+//
+//        PreparedStatement preparedStatement = connection.prepareStatement(SQLUpdate);
+//        preparedStatement.setString(1, paciente.getNome());
+//        preparedStatement.setString(2,paciente.getSobrenome());
+//        preparedStatement.setString(3,paciente.getRg());
+//        preparedStatement.setString(4,paciente.getEndereco());
+//        preparedStatement.setDate(5,java.sql.Date.valueOf(paciente.getDataCadastro()));
+//        preparedStatement.setInt(6, paciente.getIdPaciente());
+
+        String SQLUpdate = String.format("UPDATE pacientes SET nome = ?, sobrenome = ?, rg = ?, endereco = ?, dataCadastro = ?) VALUES('%s', '%s','%s','%s', '%s') WHERE idPaciiente = ?;",
+                paciente.getNome(), paciente.getSobrenome(),paciente.getRg(), paciente.getEndereco(), paciente.getDataCadastro(), paciente.getIdPaciente());
+
+        try{
+            connection = getConnection();
+            Statement statement = connection.createStatement();
+
+            statement.execute(SQLUpdate);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            connection.close();
+        }
+
+        return paciente;
     }
+
+    @Override
+    public void deletePaciente(int idPaciente) {
+
+    }
+
+    @Override
+    public Optional<Paciente> buscarPorId(int idPaciente) throws SQLException {
+        Connection connection = null;
+        String SQLBuscaPorId = "SELECT idPaciente, nome, sobrenome, rg, endereco, dataCadastro FROM pacientes WHERE idPaciente = ?";
+        Paciente paciente = null;
+
+        try{
+            connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(SQLBuscaPorId);
+            preparedStatement.setInt(1, idPaciente);
+//            Statement statement = connection.createStatement();
+
+            ResultSet resultset = preparedStatement.executeQuery();
+
+            while (resultset.next()){
+                int pKey = resultset.getInt("idPaciente");
+                String nome = resultset.getString("nome");
+                String sobrenome = resultset.getString("sobrenome");
+                String rg = resultset.getString("rg");
+                String endereco = resultset.getString("endereco");
+                LocalDate data = resultset.getDate("dataCadastro").toLocalDate();
+
+                paciente = new Paciente(pKey, nome, sobrenome, rg, endereco, data);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return Optional.ofNullable(paciente);
+    }
+
 }
