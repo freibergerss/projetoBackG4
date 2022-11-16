2e734ff94541c7ea3178078dbcfc55821f79bb79
Sabrina <freibergerssb@gmail.com>
2022-11-16 00:56:51 -0300

    Desenvolvimento do CRUD de Dentista

--- src/main/java/com/dh/projetoBackG4/dao/impl/ImplDentistaDaoH2.java
+++ src/main/java/com/dh/projetoBackG4/dao/impl/ImplDentistaDaoH2.java
@@ -2,14 +2,11 @@ package com.dh.projetoBackG4.dao.impl;
 
 import com.dh.projetoBackG4.dao.ConfigJDBC;
 import com.dh.projetoBackG4.dao.IDao;
-import model.Dentista;
+import com.dh.projetoBackG4.model.Dentista;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Optional;
 
 public class ImplDentistaDaoH2 implements IDao<Dentista> {
@@ -25,30 +22,32 @@ public class ImplDentistaDaoH2 implements IDao<Dentista> {
 
     @Override
     public Dentista create(Dentista dentista) throws SQLException {
+
         Connection connection = null;
-        String SQLInsert = String.format("INSERT INTO cadastro(nome, sobrenome, idDentista) VALUES ('%s', '%s', '%s')",
-                dentista.getNome(), dentista.getSobrenome(), dentista.getIdDentista());
+
+        String SQLInsert = String.format("INSERT INTO dentistas(nome, sobrenome) VALUES ('%s', '%s')",
+                dentista.getNome(), dentista.getSobrenome());
 
         try{
             log.info("Abrindo conexão com banco de dados");
             connection = getConnection();
             Statement statement = connection.createStatement();
 
+            log.info("Cadastrando novo dentista");
             statement.execute(SQLInsert, Statement.RETURN_GENERATED_KEYS);
-
             ResultSet resultSet = statement.getGeneratedKeys();
 
             if(resultSet.next()){
-                dentista.setIdDentista(resultSet.getInt("id"));
+                dentista.setIdDentista(resultSet.getInt("idDentista"));
             }
 
         }catch(Exception e){
 
-            log.error("Erro no banco de dados");
+            log.error("Erro ao processar banco de dados");
             e.printStackTrace();
 
         }finally{
-            log.info("Fechando a conexão com banco de dados");
+            log.info("Fechando conexão com banco de dados");
             connection.close();
         }
 
@@ -56,18 +55,71 @@ public class ImplDentistaDaoH2 implements IDao<Dentista> {
     }
 
     @Override
-    public void delete(int T) {
+    public Dentista update(Dentista dentista) throws SQLException {
+        Connection connection = null;
+
+        String UPDATE = "UPDATE dentistas SET nome=?, sobrenome=? WHERE idDentista = ?;";
+
+        try{
+            log.info("Abrindo conexão com banco de dados");
+            connection = getConnection();
+
+            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE);
+            preparedStatement.setString(1, dentista.getNome());
+            preparedStatement.setString(2, dentista.getSobrenome());
+            preparedStatement.setInt(3, dentista.getIdDentista());
+
+            log.info("Atualizando dados");
+            preparedStatement.executeUpdate();
+
+        }catch (Exception e){
+            log.error("Erro ao processar banco de dados");
+            e.printStackTrace();
+
+        }finally{
+            log.info("Fechando conexão com banco de dados");
+            connection.close();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void delete(int idDentista) {
         System.out.println("Funcionalidade não disponível");
     }
 
     @Override
-    public Optional<Dentista> buscarPorId(int T) throws SQLException {
-        return Optional.empty();
+    public Optional<Dentista> buscarPorId(int idDentista) throws SQLException {
+
+        Connection connection = null;
+
+        String QUERY = "SELECT idDentista, nome, sobrenome FROM dentistas WHERE idDentista = ?";
+
+        Dentista dentista = null;
+
+        try{
+            connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(QUERY);
+            preparedStatement.setInt(1, idDentista);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                int key = resultSet.getInt("idDentista");
+                String nome = resultSet.getString("nome");
+                String sobrenome = resultSet.getString("sobrenome");
+
+                dentista = new Dentista(nome, sobrenome, key);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //SERA QUE ADD UM FINALLY P/ FECHAR A CONEXÃO?
+
+        return Optional.ofNullable(dentista);
     }
 
-
-    @Override
-    public Dentista update(Dentista dentista) throws SQLException {
-        return null;
-    }
 }
